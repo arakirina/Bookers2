df651937f5f6e9827a5e400c37c459fa57de8a3b
RinaAraki <arakipomu1206@gmail.com>
2021-12-18 07:30:11 +0000

    [update]4-10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  
-   before_action :authenticate_user!,except: [:top]
+
+  before_action :authenticate_user!,except: [:top,:about]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -8,5 +8,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
-  
+
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,10 +27,14 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = User.new
+    @books = Book.all
   end
 
   def index
+    @book = Book.new
     @books = Book.all
+    @user = current_user
   end
 
   private
